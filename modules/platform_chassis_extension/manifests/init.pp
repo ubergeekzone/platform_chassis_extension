d194bfcb57fd9fd031c416046f7dae17f760247e
Robert O'Rourke <robert@o-rourke.org>
2020-02-06 15:24:41 +0000

    support altis nginx-additions*.conf

--- modules/platform_chassis_extension/manifests/init.pp
+++ modules/platform_chassis_extension/manifests/init.pp
@@ -2,5 +2,11 @@
 class platform_chassis_extension(
   $config
 ) {
-	# Empty, just here as a meta-package.
+	# Add an nginx config file that includes .config/nginx-additions*.conf
+	file { "/etc/nginx/sites-available/${fqdn}.d/altis-additions.nginx.conf":
+		ensure  => present,
+		content => template('platform_chassis_extension/nginx.conf'),
+		notify  => Service['nginx'],
+		require => File["/etc/nginx/sites-available/${fqdn}.d"],
+	}
 }
