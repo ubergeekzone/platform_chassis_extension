f62b3802d05a8835fe1c4c330e83cd8e9632b866
Robert O'Rourke <robert@o-rourke.org>
2020-02-06 15:49:28 +0000

    use a simple string template instead

--- modules/platform_chassis_extension/manifests/init.pp
+++ modules/platform_chassis_extension/manifests/init.pp
@@ -5,7 +5,7 @@ class platform_chassis_extension(
 	# Add an nginx config file that includes .config/nginx-additions*.conf
 	file { "/etc/nginx/sites-available/${fqdn}.d/altis-additions.nginx.conf":
 		ensure  => present,
-		content => template('platform_chassis_extension/nginx.conf'),
+		content => 'include /chassis/.config/nginx-additions*.conf;',
 		notify  => Service['nginx'],
 		require => File["/etc/nginx/sites-available/${fqdn}.d"],
 	}
